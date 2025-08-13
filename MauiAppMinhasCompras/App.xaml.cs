e15141cc2e60c8ee76b9a3abc5281740f17c0fb3
vanremo <vanremo23@yahoo.com>
2025-08-13 20:15:37 -0300

    construção de CRUD para Sqlite

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -6,9 +6,8 @@
         {
             InitializeComponent();
 
-            // MainPage = new AppShell();
+            //MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
-        
     }
 }
--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -2,8 +2,8 @@ namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
 {
-	public EditarProduto()
-	{
-		InitializeComponent();
-	}
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
 }
